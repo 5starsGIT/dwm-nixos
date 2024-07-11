4f8a52ddd2445f1ea86793af80176b0902d59f89
Alexandre Bennett <amradiosucks87@gmail.com>
2024-07-10 22:48:54 -0400

    fixes

--- config.h
+++ config.h
@@ -2,7 +2,7 @@
 #include <X11/XF86keysym.h>
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
@@ -70,9 +70,9 @@ static const char *mutevol[]    = { "wpctl",   "set-mute",   "@DEFAULT_AUDIO_SIN
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbgcolor, "-sf", selfgcolor, NULL };
 static const char *termcmd[]  = { "kitty", NULL };
-static const char *desktop_demnu_cmd[] = { "/home/alex/Documents/Scripts/desktop-dmenu", NULL };
+static const char *desktop_demnu_cmd[] = { "/home/alex/Documents/Scripts/desktop-dmenu.sh", NULL };
 static const char *filecmd[] = { "nemo", NULL };
-static const char *screenshot[] = { "/home/alex/Documents/Scripts/takeScreenShot", NULL };
+static const char *screenshot[] = { "/home/alex/Documents/Scripts/takeScreenShot.sh", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
