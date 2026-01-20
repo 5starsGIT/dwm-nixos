dd0a6bb96c71531cecf55b4257d307af4d2cc60a
Alexandre Bennett <amradiosucks87@gmail.com>
2026-01-20 18:14:29 -0500

    addedalwaysontop

--- config.h
+++ config.h
@@ -6,8 +6,8 @@ static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Firacode Nerd Font:size=12" };
-static const char dmenufont[]       = "Firacode Nerd Font:size=12";
+static const char *fonts[]          = { "monospace:size=12" };
+static const char dmenufont[]       = "monospace:size=12";
 static const char selfgcolor[]      = "#191724";
 static const char normbgcolor[]     = "#1f1d2e";
 static const char normbordercolor[] = "#26233a";
@@ -96,6 +96,7 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
+	{ MODKEY|ShiftMask,             XK_o,  togglealwaysontop, {0} },
 	{ MODKEY|ShiftMask,             XK_f,      togglefullscr,  {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
 	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
